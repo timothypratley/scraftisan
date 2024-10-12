12c4a3a2b8734090e743ad0982e7545388a147eb
Timothy Pratley <timothypratley@gmail.com>
2024-10-12 16:56:35 -0700

    adding layout slides

--- src/scraftisan/layout.clj
+++ src/scraftisan/layout.clj
@@ -1,7 +1,33 @@
 (ns scraftisan.layout
-  (:require [scraftisan.util :as util]))
+  (:require [scraftisan.marcup :as marcup]
+            [scraftisan.util :as util]))
 
-(def layouts)
+(def layouts
+  (marcup/mo "Layouts
+
+* Manual
+* Automatic
+* Mixed"))
+
+(def automatic-layouts
+  (marcup/mo "Should be easy right?
+
+Unsolved, but some interesting takes:
+
+* trees
+* gravity/charge simulations
+* constraint solvers
+"
+
+             ))
+
+(def libraries
+  (marcup/mo "Libraries
+
+* Eclipse Layout Kernel
+* Webcola"))
 
 (def slides
-  (util/arrange [layouts]))
+  (util/arrange [layouts 0 0]
+                [automatic-layouts 400 0]
+                [libraries 800 0]))
